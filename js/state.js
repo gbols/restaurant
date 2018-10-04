9b25dc020ec319d9391de6e9980b77a18e7f96fe
olagunju gbolahan <gbolahanpaul@gmail.com>
2018-10-04 03:47:45 +0100

    [feature #[160926542]] Add funtionality to load menu items from the database

--- js/state.js
+++ js/state.js
@@ -3,12 +3,22 @@ const signoutState = document.querySelectorAll(".signout-state");
 const userDetails = document.getElementById("display-name");
 const navUserDetails = document.getElementById("nav-display-name");
 
+
+ 
+/**
+ * sets the display property to none or block 
+ * @param  {NodeList} elements
+ * @param  {string} value
+ */
 const changeState = (elements, value) => {
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.display = value;
   }
 };
 
+/** 
+ * checks if a user is logged and update app state accordingly
+*/
 if (localStorage.getItem("token")) {
   changeState(signoutState, "inline");
   changeState(state, "none");
@@ -30,6 +40,13 @@ if (localStorage.getItem("token")) {
   }
 }
 
+
+/**
+ * validate!
+ * @param  {string} password
+ * @param  {string} confirmPassword
+ * @returns {boolean}
+ */
 const validate = (password, confirmPassword) => {
   if (password !== confirmPassword) return false;
   return true;
